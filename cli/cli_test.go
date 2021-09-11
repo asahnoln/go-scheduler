1f9d04fd7bd82b23b3826f2185fc1edee075d2ca
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-09-11 21:47:20 +0600

    Removed redundant CLI tests
    - [REMOVED] Extra tests which are covered by more complex tests

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -11,61 +11,6 @@ import (
 	"github.com/asahnoln/go-scheduler/test"
 )
 
-func TestCLIShowData(t *testing.T) {
-	in := strings.NewReader(`
-add apollo monday 09:00-14:00 15:00-18:00
-add apollo monday 16:00-19:00
-add apollo tuesday 12:00-13:00
-show apollo
-quit
-`[1:])
-	out := bytes.Buffer{}
-
-	c := cli.NewCLI(in, &out)
-
-	err := c.MainLoop()
-	test.AssertNoError(t, err, "unexpected error while processing CLI: %v")
-
-	want := `
-apollo
-
-Monday
-09:00-14:00
-15:00-19:00
-
-Tuesday
-12:00-13:00
-
-`[1:]
-
-	test.AssertSameString(t, want, out.String(), "want output\n%v\n\ngot\n%v")
-}
-
-func TestCLIAddDifferentItems(t *testing.T) {
-	names := []string{
-		"arthur", "apollo",
-	}
-
-	for _, n := range names {
-		t.Run("get "+n+" schedule", func(t *testing.T) {
-			in := strings.NewReader("add " + n + " thursday 14:25-15:15\nshow " + n + "\nquit")
-			out := &bytes.Buffer{}
-
-			c := cli.NewCLI(in, out)
-			err := c.MainLoop()
-			test.AssertNoError(t, err, "unexpected error while processing CLI: %v")
-
-			want := n + `
-
-Thursday
-14:25-15:15
-
-`
-			test.AssertSameString(t, want, out.String(), "want output\n%v\ngot\n%v")
-		})
-	}
-}
-
 func TestCliWholeWeek(t *testing.T) {
 	days := map[time.Weekday]string{
 		time.Monday:    "monday",
